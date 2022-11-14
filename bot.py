fb08de374ccad18d481a119bb03f352806ad7381
Jordy Kafwe <kfwjor001@myuct.ac.za>
2022-11-14 22:34:56 +0200

    Add create tweet from post

--- bot.py
+++ bot.py
@@ -5,3 +5,16 @@ response = requests.get(
     headers={"user-agent": "scraping posts for @CodingMemesBot twitter bot"},
 ).json()
 reddit_posts = response["data"]["children"]
+
+
+def create_tweet(post):
+    """Creates a tweet from a reddit post"""
+
+    tweet = {
+        "title": post["data"]["title"],
+        "author": f'u/{post["data"]["author"]}',
+        "post_url": f'www.reddit.com/{post["data"]["permalink"]}',
+        "image_url": post["data"]["url"],
+    }
+
+    return tweet
