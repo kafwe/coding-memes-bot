96cd6cdf1aac0b364787a0f50da76b845df39db8
Jordy Kafwe <kfwjor001@myuct.ac.za>
2022-11-29 19:13:09 +0200

    Seperate fetching posts and tweeting

--- bot.py
+++ bot.py
@@ -1,35 +1,54 @@
 import requests
+import tweepy
+from PIL import Image
+from io import BytesIO
+import os
 
-response = requests.get(
-    "https://www.reddit.com/r/ProgrammerHumor/top.json",
-    headers={"user-agent": "scraping posts for @CodingMemesBot twitter bot"},
-).json()
-reddit_posts = response["data"]["children"]
+
+auth = tweepy.OAuthHandler(
+    os.environ["TWITTER_CONSUMER_KEY"], os.environ["TWITTER_CONSUMER_SECRET"]
+)
+auth.set_access_token(
+    os.environ["TWITTER_ACCESS_TOKEN"], os.environ["TWITTER_ACCESS_TOKEN_SECRET"]
+)
+
+api = tweepy.API(auth)
 
 
 def create_tweet(post):
     """Creates a tweet from a reddit post"""
 
     tweet = {
-        "title": post["data"]["title"],
-        "author": f'u/{post["data"]["author"]}',
-        "post_url": f'www.reddit.com/{post["data"]["permalink"]}',
-        "image_url": post["data"]["url"],
+        "id": post[0],
+        "text": post[1],
+        "author": post[2],
+        "subreddit": post[3],
+        "reddit_post_url": post[4],
+        "image_url": post[5],
     }
 
     return tweet
 
 
-def is_valid(post):
-    """Checks if a reddit post is valid for a tweet.
-    That is the post type is Meme and it has an image."""
+def tweet_meme(tweet):
+    """Tweets a meme"""
 
-    is_text = post["data"]["is_self"]
+    response = requests.get(tweet["image_url"])
+    image = Image.open(BytesIO(response.content))
+    filename = tweet["image_url"].split("/")[-1]
+    image.save(f"tmp/{filename}")
+    media = api.media_upload(filename=f"tmp/{filename}")
+    api.update_status(status=tweet["title"], media_ids=[media.media_id])
 
-    if is_text:
-        return False
 
-    is_image = post["data"]["url"].endswith((".jpg", ".png"))
-    is_meme = post["data"]["link_flair_text"] == "Meme"
+def tweet_meme_info(tweet):
+    """Tweets meme info. This is a reply to the meme tweet
+    containing the author and subreddit"""
 
-    return is_meme and is_image
+    last_tweet = api.user_timeline(count=1)[0]
+
+    api.update_status(
+        f'Posted by {tweet["author"]} in {tweet["subreddit"]}\
+        \n\n{tweet["reddit_post_url"]}',
+        in_reply_to_status_id=last_tweet.id,
+    )
---
+++ fetch_posts.py
@@ -0,0 +1,56 @@
+import requests
+import html
+
+
+def main():
+    reddit_posts = get_posts("ProgrammerHumor")
+
+    for reddit_post in reddit_posts:
+        if is_valid(reddit_post):
+            post = format_post(reddit_post)
+
+
+def get_posts(subreddit):
+    """Gets posts from Reddit API"""
+
+    response = requests.get(
+        f"https://www.reddit.com/r/{subreddit}/top.json?limit=15",
+        headers={"user-agent": "scraping posts for @CodingMemesBot twitter bot"},
+    )
+
+    if response.status_code != 200:
+        raise Exception("Reddit API request unsuccessful")
+
+    response = response.json()
+    reddit_posts = response["data"]["children"]
+    return reddit_posts
+
+
+def is_valid(post):
+    """Checks if a reddit post is valid for a tweet.
+    That is the post type is Meme and it has an image"""
+
+    is_text = post["data"]["is_self"]
+
+    if is_text:
+        return False
+
+    is_image = post["data"]["url"].endswith((".jpg", ".png"))
+    is_meme = post["data"]["link_flair_text"] == "Meme"
+
+    return is_meme and is_image
+
+
+def format_post(post):
+    """Formats a reddit post for a tweet"""
+
+    post = {
+        "id": post["data"]["id"],
+        "text": html.unescape(post["data"]["title"]),
+        "author": f'u/{post["data"]["author"]}',
+        "subreddit": f'r/{post["data"]["subreddit"]}',
+        "reddit_post_url": f'www.reddit.com/{post["data"]["permalink"]}',
+        "image_url": post["data"]["url"],
+    }
+
+    return post
