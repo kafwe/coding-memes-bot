e7bd5c40ff7d80cfa761dc7ccf5465f50f7c16b2
Jordy Kafwe <jordykafwe@gmail.com>
2023-08-20 01:51:22 +0200

    Upgrade bot to Twitter v2 API

--- bot/bot.py
+++ bot/bot.py
@@ -28,7 +28,7 @@ except Exception as e:
     logger.error(e)
     sys.exit()
 
-# Connect to Twitter
+# Twitter V1 API client for uploading images
 auth = tweepy.OAuthHandler(
     os.environ["TWITTER_CONSUMER_KEY"], os.environ["TWITTER_CONSUMER_SECRET"]
 )
@@ -39,14 +39,22 @@ auth.set_access_token(
 
 api = tweepy.API(auth)
 
+# Twitter API V2 client for creating tweets
+client = tweepy.Client(
+    consumer_key=os.environ["TWITTER_CONSUMER_KEY"], 
+    consumer_secret=os.environ["TWITTER_CONSUMER_SECRET"],
+    access_token=os.environ["TWITTER_ACCESS_TOKEN"], 
+    access_token_secret=os.environ["TWITTER_ACCESS_TOKEN_SECRET"]
+)
+
 
 def lambda_handler(event, context):
     post = get_post()
 
     if post:
         tweet = create_tweet(post)
-        tweet_meme(tweet)
-        tweet_meme_info(tweet)
+        tweet_id = tweet_meme(tweet)
+        tweet_meme_info(tweet, tweet_id)
         update_post(tweet)
         logger.info(f"Tweeted post {tweet['id']}")
     else:
@@ -70,26 +78,25 @@ def create_tweet(post):
 
 
 def tweet_meme(tweet):
-    """Tweets a meme"""
+    """Tweets a meme and returns the tweet id."""
 
     response = requests.get(tweet["image_url"])
     image = Image.open(BytesIO(response.content))
     filename = tweet["image_url"].split("/")[-1]
     image.save(f"/tmp/{filename}")
     media = api.media_upload(filename=f"/tmp/{filename}")
-    api.update_status(status=tweet["text"], media_ids=[media.media_id])
+    tweet_response = client.create_tweet(text=tweet["text"], media_ids=[media.media_id])
+    return tweet_response.data["id"]
 
 
-def tweet_meme_info(tweet):
+def tweet_meme_info(tweet, last_tweet_id):
     """Tweets meme info. This is a reply to the meme tweet
     containing the author and subreddit"""
 
-    last_tweet = api.user_timeline(count=1)[0]
-
-    api.update_status(
-        f'Posted by {tweet["author"]} in {tweet["subreddit"]}\
+    client.create_tweet(
+        text=f'Posted by {tweet["author"]} in {tweet["subreddit"]}\
         \n\n{tweet["reddit_post_url"]}',
-        in_reply_to_status_id=last_tweet.id,
+        in_reply_to_tweet_id=last_tweet_id,
     )
 
 
